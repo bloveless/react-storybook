0a286fe78a24f4130d727fafdf3adc39c4acf260
Brennon Loveless <brennon.loveless@gmail.com>
2018-08-26 01:42:46 -0600

    Got the calendar working with random values.

--- stories/calendar-heatmap.js
+++ stories/calendar-heatmap.js
@@ -1,46 +1,42 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { getDaysInMonth } from 'date-fns';
 import CalendarHeatmap from '@bloveless/react-calendar-heatmap/CalendarHeatmap';
 
 import '@bloveless/react-calendar-heatmap/css/CalendarHeatmap.css';
 
 storiesOf('Calendar Heatmap', module)
   .add('Default', () => {
-    const heatmapData = [
-      {
-        date: '2018-08-14',
-        value: 40,
-      },
-      {
-        date: '2018-08-15',
-        value: 30,
-      },
-      {
-        date: '2018-08-16',
-        value: 50,
-      },
-      {
-        date: '2018-08-17',
-        value: 10,
-      },
-      {
-        date: '2018-08-18',
-        value: 100,
-      },
-      {
-        date: '2018-08-19',
-        value: 200,
-      },
-      {
-        date: '2018-08-20',
-        value: 250,
-      },
-    ];
+    const startDate = '2018-08-31';
+    const getData = (newDate) => {
+      action('month-changed')(newDate);
+
+      const newData = [];
+
+      // Get the data for the newDate.
+      const newMonth = newDate.getMonth() + 1;
+      const newYear = newDate.getFullYear();
+      const dateBase = `${newYear}-${String(newMonth).padStart(2, '0')}`;
+
+      // For each day in the month generate a random number between 0 - 250.
+      for (let i = 1; i <= getDaysInMonth(newDate); i += 1) {
+        const currentDate = `${dateBase}-${String(i).padStart(2, '0')}`;
+        newData.push({
+          date: currentDate,
+          value: Math.floor(Math.random() * Math.floor(250)),
+        });
+      }
+
+      console.log('getdata', newData);
+
+      return newData;
+    };
 
     return (
       <CalendarHeatmap
-        currentDate="2018-08-18T12:00:00"
-        data={heatmapData}
+        startDate={startDate}
+        getData={getData}
         height={500}
         width={500}
       />
